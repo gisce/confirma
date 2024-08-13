3ae5f66af9e7a6b8e7a2c387cd7f7249b56439bc
PSala <polsalalidon@gmail.com>
2024-08-13 10:17:19 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     url='https://github.com/gisce/confirma',
     license='MIT',
     author='GISCE-TI, S.L.',
+    long_description='''Send Documents to ConFirma service package''',
     author_email='devel@gisce.net',
     description='Send Documents to ConFirma service',
     install_requires=INSTALL_REQUIRES
