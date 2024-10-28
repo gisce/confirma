0ea10e304ebbbee6a8b15d04e69c4c405244bc5a
giscegit <devel@gisce.net>
2024-10-28 09:50:46 +0000

    Bump to v0.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='confirma',
-    version='0.2.1',
+    version='0.2.2',
     packages=find_packages(),
     url='https://github.com/gisce/confirma',
     license='MIT',
