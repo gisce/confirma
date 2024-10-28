296fd74cc0b53edec7466333f2c2b7f47ec603c3
PSala <polsalalidon@gmail.com>
2024-10-28 15:08:26 +0100

    fix reqs

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt', 'r') as f:
-    INSTALL_REQUIRES = f.readlines()
-
 setup(
     name='confirma',
     version='0.2.2',
@@ -13,5 +10,7 @@ setup(
     long_description='''Send Documents to ConFirma service package''',
     author_email='devel@gisce.net',
     description='Send Documents to ConFirma service',
-    install_requires=INSTALL_REQUIRES
+    install_requires=[
+        'requests'
+    ],
 )
